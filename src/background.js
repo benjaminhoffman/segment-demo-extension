1a9f3f319b8761106d65b4aec27f2a593aaaeffc
jpcarriero <johncarriero@Johns-MacBook-Pro.local>
2016-07-14 06:52:04 -0700

    not looking pretty but hover and click works

--- src/background.js
+++ src/background.js
@@ -3,50 +3,20 @@ console.log("background.js")
 //const Analytics = require('./analytics-node.js');
 //const analytics = new Analytics('cuOgSJgFEt1tN3lSUEfvWv6g1NM1hXmY', { flushAt: 1 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == 'events');
 
-  port.onMessage.addListener(function(msg) {
-    chrome.storage.sync.get(null, function (localStorage) {
-      console.log(localStorage)
-      if (localStorage[msg.url]) {
-        port.postMessage(localStorage[msg.url]);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.log(message);
+  if (message.from === 'content') {
+    var popupWindows = chrome.extension.getViews({type:'popup'});
+    if (popupWindows.length) { // A popup has been found
+        popupWindows[0].customize_event(message.subject);
+    }
+    else{
+      chrome.tabs.create({url:"popup.html"});
+      var popupWindows = chrome.extension.getViews({type:'popup'});
+       if (popupWindows.length) { // A popup has been found
+        popupWindows[0].customize_event(message.subject);
       }
-      else if(localStorage[msg.url+'?customize']){
-        port.postMessage(localStorage[msg.url+'?customize']);
-      }
-    });
-
-    // clears out chrome's localStorage
-    if (msg.clear) {
-      chrome.storage.sync.clear(function() {
-        alert('ALL event data cleared')
-      });
     }
-
-    // creates a new event on form submit (popup.html)
-    if (msg.newEvent) {
-      const newEvent = {};
-      newEvent[msg.newEvent.url] = msg.newEvent.trackCall;
-      chrome.storage.sync.set(newEvent, function() {
-        console.log('Event Created!')
-      });
-    }
-
-    if (msg.customize){
-      const newEvent = {};
-      chrome.extension.getBackgroundPage().console.log(msg.customize.url)
-      newEvent[msg.customize] = true;
-      chrome.storage.sync.set(newEvent, function() {
-        chrome.extension.getBackgroundPage().console.log('customize hit background page')
-      });
-      
-    }
-    // sends data to segment onClick
-    if (msg.trackEvent) {
-      console.log('track event sent to Segment')
-      //analytics.identify({ userId: msg.trackEvent.userId });
-      //analytics.track(msg.trackEvent);
-    }
-  });
+  }
 });
--- src/content.js
+++ src/content.js
@@ -1,41 +1,45 @@
 console.log('content.js')
+var customized_event = {};
+var current_ele = '';
 
-const trackEvent = {};
-const port = chrome.runtime.connect({ name: "events" });
-
-port.postMessage({ url: window.location.href });
-
-port.onMessage.addListener(function(msg,trackEvent) {
-	console.log(msg)
-	if(msg){
-		var link = document.createElement( "link" );
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+  console.log(msg.from);
+  console.log(msg.subject);
+  if ((msg.from === 'popup') && (msg.subject === 'customize')) {
+  	var link = document.createElement( "link" );
 		link.href = chrome.extension.getURL('src/hover.css');
 		link.type = "text/css";
 		link.rel = "stylesheet";
 		link.media = "screen,print";
 		document.getElementsByTagName( "head" )[0].appendChild( link );
-		var overlay = document.createElement('div');
-		document.body.appendChild(overlay);
-		overlay.id = "add_event"
-		overlay.class = "overlay"
-		overlay.innerHTML = '<img src="https://cdn3.iconfinder.com/data/icons/interface/100/close_button_1-512.png" class="closebtn" onclick="function(){document.getElementById(\'add_event\').style.width = \'0%\'}"><div class="overlay-content"><div>Event Name:<input type="text" name="event_name" value="" maxlength="100" /><button onclick="function(){document.getElementById(\'add_event\').style.width = \'0%\'}>Done</button></div></div>'
+  	var info = function(ele) {
+  		return function() {
+  			console.log('element_clicked');
+    		chrome.runtime.sendMessage({
+      			from:    'content',
+      			subject: 'id: '+ele.id+':name: '+ele.name
+    		});
+  		}
+	};
 
-		var all = document.getElementsByTagName("*");
-		for (var i=0, max=all.length; i < max; i++) {
-     		all[i].onclick=function(){
-     			document.getElementById("add_event").style.width = "100%";
-     		}
-		}
-		overlay.onclick=null;
-		var div = document.createElement('div');
-		document.body.appendChild(div);
-		div.textContent = 'Tagging Mode';
-		div.className = 'edit_mode';
+	var all = document.getElementsByTagName("div");;
+	for (var i = 0, l = all.length; l > i; i++){
+  		all[i].onmouseover=function(){current_ele = this.id; console.log(this.id);};
 	}
-	else{
-	  document.body.addEventListener('click', function() {
-	    console.log(trackEvent)
-	    port.postMessage({ trackEvent: trackEvent })
-	  });
-	}
+  }
+
+  	var div = document.createElement('div');
+	document.body.appendChild(div);
+	div.innerHTML = '<span class="edit_bar_cell">Edit Mode: <span> <div class="edit_bar_cell" id="current_hover"></div><label class="edit_bar_cell">Tracking Call to Assign</label><input class="edit_bar_cell" type="text" placeholder="product viewed"><button class="edit_bar_cell" id="save">save</button>'
+	div.className = 'edit_mode';
+
+	document.body.addEventListener('click', function() {
+	    console.log(current_ele);
+	    document.getElementById("current_hover").innerHTML = current_ele;
+	    for (var i = 0, l = all.length; l > i; i++){
+  			all[i].onmouseover=null;
+  		}
+	});
+
+	
 });
